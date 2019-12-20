bf3886a016200e3f9b1e3cba9faecd4119bab5fa
Will Carter <will.carter@fergusllc.com>
2019-12-20 01:52:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,14 @@
 # Write your code here.
 def line(current_line)
-  formatted = []
-  current_line.each_with_index do |person, index|
-    formatted.push("#{index + 1}. #{person}")
+  if(current_line.length == 0)
+    puts "The line is currently empty."
+  else
+    formatted = []
+    current_line.each_with_index do |person, index|
+      formatted.push("#{index + 1}. #{person}")
+    end
+    puts "The line is currently: " + formatted.join(" ")
   end
-  puts "The line is currently: " + formatted.join(" ")
 end
 
 katz_deli = ["Ada", "Grace", "Kent"]
