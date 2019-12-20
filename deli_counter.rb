a180a7825fc9de87192644a6a44d75e1b619665e
Will Carter <will.carter@fergusllc.com>
2019-12-20 01:48:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,9 @@
 # Write your code here.
 def line(current_line)
-  formatted = current_line
+  formatted = []
+  current_line.each_with_index do |person, index|
+    puts "#{index}, #{person}"
+  end
 end
 
 katz_deli = ["Ada", "Grace", "Kent"]
