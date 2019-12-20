44672184e1740df6bd5423838ed66f355b54ae39
Will Carter <will.carter@fergusllc.com>
2019-12-20 02:04:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,6 +16,17 @@ def take_a_number(current_line, name)
   puts "Welcome, #{name}. You are number #{current_line.length} in line."
 end
 
+def now_serving(current_line)
+  if(current_line.length == 0)
+    puts "There is nobody waiting to be served!"
+  else
+    person = current_line[0]
+    current_line.shift[]
+    puts "Currently serving #{person}."
+  end
+end
+
 katz_deli = ["Ada", "Grace", "Kent"]
 #line(katz_deli)
-take_a_number(katz_deli, "Bob")
+#take_a_number(katz_deli, "Bob")
+now_serving(katz_deli)
